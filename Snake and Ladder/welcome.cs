a929743b019331c08fc9057f852cf4d046de7032
duvvamounika <mounikaduvva05@gmail.com>
2022-04-05 16:22:27 +0530

    ADD welcome program

---
+++ Snake and Ladder/welcome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder game");
+            Console.ReadLine();
+        }
+    }
+}
